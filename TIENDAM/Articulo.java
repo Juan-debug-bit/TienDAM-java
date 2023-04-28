0fa89f8afc46824bedb8fa90d48d16dd9fa3413d
Juan Pablo Jimenez Franco <juanp.jimenezfranco@gmail.com>
2023-04-28 22:44:59 +0200

    Finalización de clase articulo

--- TIENDAM/Articulo.java
+++ TIENDAM/Articulo.java
@@ -1,5 +1,45 @@
 package TIENDAM;
 
 public class Articulo {
+    final String nombre;
+    double precio;
+    final double iva;
+    int cantidad;
+
+    public Articulo(String nombre, double precio, double iva, int cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.iva = iva;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String toString() {
+        return "Articulo [nombre= " + nombre + ", precio= " + precio + ", iva= " + iva + ", cantidad= " + cantidad + "]";
+    }
     
+
 }
