db78a852892b3cd055a4a662a246a8f471d6fcdf
Juan Pablo Jimenez Franco <juanp.jimenezfranco@gmail.com>
2023-04-28 23:08:04 +0200

    añadiendo metodos sin terminar a la clase Almacen

--- TIENDAM/Almacen.java
+++ TIENDAM/Almacen.java
@@ -1,5 +1,27 @@
 package TIENDAM;
 
 public class Almacen {
-    
+    final int MaxArticulos=100;
+    Articulo [] articulos = new Articulo [MaxArticulos];
+    int cantidad;
+
+    public  boolean anyadir (boolean a) {
+        return a;
+        
+    }
+    public boolean anyadirArticulo() {
+        return false;
+    }
+    public boolean quitarArticulo(int cantidad) {
+        return false;
+    }
+    public boolean ModificarPrecio (int cantidad, int precio) {
+        return false;
+    }
+    public boolean recibir (int cantidad, int precio) {
+        return false;
+    }
+    public boolean devolver (int cantidad, int precio) {
+        return false;
+    }
 }
